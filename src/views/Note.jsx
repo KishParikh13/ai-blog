9b1296db378e408a46c9d32631006aadb49962a1
KishParikh13 <kaparikh@mail.smu.edu>
2024-04-27 17:39:58 -0400

    suggestion responsiveness

--- src/views/Note.jsx
+++ src/views/Note.jsx
@@ -193,7 +193,7 @@ function Note(props) {
                 <button onClick={generateSuggestions} className='bg-green-700 hover:bg-green-800 text-white px-4 py-2 rounded-md text-lg'>
                     Ask AI
                 </button>
-                <div className='grid grid-cols-3 gap-4 mt-4'>
+                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-4'>
                     {
                         note.suggestions && note.suggestions.map((suggestion, i) => {
                             return (
